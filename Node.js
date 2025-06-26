46ea73241633b33d91971665e6faf83ddb44b697
Sohan <sohan291106@gmail.com>
2025-06-26 15:28:52 +0530

    added api to update book data

--- Node.js
+++ Node.js
@@ -24,11 +24,11 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("welcome to Sohan's Library");
 })
-
+//to get all books data
 app.get('/books', (_, res) => {
     res.send(booksData);
 })
-
+//to add a book
 app.post('/books', (req, res) => {
     booksData.forEach(book => {
         if (book.id === req.body.id) {
@@ -38,6 +38,22 @@ app.post('/books', (req, res) => {
     booksData.push(req.body);
     res.status(200).send({ message: "Book added successfully" });
     console.log(booksData)
+})
+//to update a book
+app.put('/books/:id', (req, res) => {
+    const id = req.params.id;
+    const reqId = req.body.id;
+    if (parseInt(id) !== parseInt(reqId)) {
+        return res.status(400).send({ message: `Book id mismatch` });
+    }
+    booksData.find(book => {
+        if (book.id === parseInt(id)) {
+            book.title = req.body.title;
+            book.author = req.body.author;
+            return res.status(200).send({ message: "Book updated successfully", data: booksData });
+        }
+    })
+    res.status(404).send({ message: "Book not found" });
 })
 
 app.listen(port, () => {
