7bef33c7037ce50771576655bcad85eb0b91ed55
Sohan <sohan291106@gmail.com>
2025-06-26 12:14:30 +0530

    created a get api to fetch books data

--- Node.js
+++ Node.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const booksData = [
+    {
+        "id": 1,
+        "title": "Book-1",
+        "author": "Author-1",
+    },
+    {
+        "id": 2,
+        "title": "Book-2",
+        "author": "Author-2",
+    },
+    {
+        "id": 3,
+        "title": "Book-3",
+        "author": "Author-3",
+    }
+];
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("welcome to Sohan's Library");
+})
+
+app.get('/books', (req,res) => {
+    res.send(booksData);
 })
 
 app.listen(port, () => {
